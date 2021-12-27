6cec25906758d6c460340baadeb0d605aa1daf18
NaYeonGong <n_yeon6347@naver.com>
2021-12-27 16:52:44 +0900

    Create 입양시각구하기2.sql

---
+++ SQL_level4/입양시각구하기2.sql
@@ -0,0 +1,9 @@
+-- 0시부터 23시까지, 각 시간대별로 입양이 몇 건 발생했는지 조회.
+-- 결과는 시간대 순으로 정렬
+
+SET @HOUR = -1;
+SELECT (@HOUR := @HOUR + 1) AS HOUR,
+        (SELECT COUNT(HOUR(DATETIME)) FROM ANIMAL_OUTS
+         WHERE HOUR(DATETIME) = @HOUR) AS COUNT
+FROM ANIMAL_OUTS
+WHERE @HOUR < 23;
