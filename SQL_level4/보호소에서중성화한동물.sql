f7ccbce900902f073f71993410de4a1b6a5a0cfc
NaYeonGong <n_yeon6347@naver.com>
2021-12-28 19:45:47 +0900

    Create 보호소에서중성화한동물.sql

---
+++ SQL_level4/보호소에서중성화한동물.sql
@@ -0,0 +1,9 @@
+-- 보호소 들어올 당시에는 중성화되지 않았지만,
+-- 보호소를 나갈 당시에는 중성화된 동물의 아이디, 생물종, 이름 조회
+-- 아이디 순으로 조회
+SELECT O.ANIMAL_ID, O.ANIMAL_TYPE, O.NAME FROM ANIMAL_OUTS O
+LEFT OUTER JOIN ANIMAL_INS I
+ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE I.SEX_UPON_INTAKE LIKE '%Intact%'
+AND (O.SEX_UPON_OUTCOME LIKE '%Spayed%' OR O.SEX_UPON_OUTCOME LIKE '%Neutered%')
+ORDER BY O.ANIMAL_ID
