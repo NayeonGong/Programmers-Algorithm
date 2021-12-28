4f6a72d822abb5fe260fb9f5ffea9c8e536f053a
NaYeonGong <n_yeon6347@naver.com>
2021-12-28 18:49:16 +0900

    Create 루시와엘라찾기.sql

---
+++ SQL_level2/루시와엘라찾기.sql
@@ -0,0 +1,4 @@
+-- 동물 중 이름이 'Lucy','Ella','Pickle','Rogan','Sabrina','Mitty'인 동물의 
+-- 아이디, 이름, 성별 및 중성화 여부 조회
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE FROM ANIMAL_INS
+WHERE NAME IN ('Lucy','Ella','Pickle','Rogan','Sabrina','Mitty')
